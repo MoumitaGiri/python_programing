f87ba2c34b141fe1bca8ce824b78dffeea33306c
Moumita Giri <girimoumita579@gmail.com>
2022-11-21 19:48:33 +0530

    list insertion method

---
+++ insertlist.py
@@ -0,0 +1,16 @@
+list = ["a", "b","c","d"]
+list.insert(2, "e")
+print(list)
+
+
+mylist = [1, 79, 56, 54]
+mylist.insert(3, 89)
+print(mylist)
+
+
+arr1 = [3, 4, 6, 7, 9]
+arr2 = [1, 2, 5, 8, 0]
+
+arr1.extend(arr2)
+print(arr1)
+
