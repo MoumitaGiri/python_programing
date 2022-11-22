2da304da13672a5d0cdee1744b668a619a8a1f3f
Moumita Giri <girimoumita579@gmail.com>
2022-11-22 19:29:36 +0530

    sort list

--- sort.py
+++ sort.py
@@ -1,7 +1,21 @@
-#l = [ "a", "t", "u", "k", "H"]
-#l.sort()
-#print(l)
 
-l = (input("enter the alphabet"))
-l.sort(reverse = True)
+l = [ "a", "t", "u", "k", "H"]
+l.sort()
 print(l)
+
+list = [int(y) for y in input().split()]
+list.sort()
+print(list)
+
+list = [i for i in input().split()]
+list.sort()
+print(list)
+
+list = [str(x) for x in input().split()]
+list.sort()
+print(list)
+
+list = [int(x) for x in input().split()]
+list.sort()
+print(list)
+
